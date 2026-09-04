faf7b85e73a7ad6f14581d7a50cdbf363efae496
Cedryck Paquette <e2129359@cmaisonneuve.qc.ca>
2026-09-04 14:58:12 -0400

    Début internalisation Création de fichier JSON pour textes

--- commun/entete.inc.php
+++ commun/entete.inc.php
@@ -1,3 +1,13 @@
+<?php
+//Lire le fichier JSON contenant les textes
+$textesJSON = file_get_contents("i18n/textes-fr.json");
+//test
+echo $textesJSON;
+//Convertir la chaine JSON en structure PHP
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -8,8 +18,8 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>teeTIM // fibre naturelle ... conception artificielle</title>
-    <meta name="description" content="Page d'accueil du concepteur de vêtements 100% fait au Québec, conçus par les étudiants du TIM à l'aide de designs produits par intelligence artificielle, et fabriqués avec des fibres 100% naturelles et biologiques.">
+    <title></title>
+    <meta name="description" content="">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" type="image/png" href="images/favicon.png" />
 </head>
@@ -24,18 +34,18 @@
             </nav>
             <nav class="barre-logo">
                 <label for="cc-btn-responsive" class="material-icons burger">menu</label>
-                <a class="logo" href="index.php"><img src="images/logo.png" alt="Accueil"></a>
+                <a class="logo" href="index.php"><img src="images/logo.png" alt=""></a>
                 <a class="material-icons panier" href="panier.php">shopping_cart</a>
-                <input class="recherche" type="search" name="motscles" placeholder="Recherche">
+                <input class="recherche" type="search" name="motscles" placeholder="">
             </nav>
             <input type="checkbox" id="cc-btn-responsive">
             <nav class="principale">
                 <label for="cc-btn-responsive" class="menu-controle material-icons">close</label>
-                <a href="teeshirts.php">Teeshirts</a>
-                <a href="casquettes.php">Casquettes</a>
-                <a href="hoodies.php">Hoodies</a>
+                <a href="teeshirts.php"></a>
+                <a href="casquettes.php"></a>
+                <a href="hoodies.php"></a>
                 <span class="separateur"></span>
-                <a href="aide.php">Aide</a>
-                <a href="apropos.php">À propos de nous</a>
+                <a href="aide.php"></a>
+                <a href="apropos.php"></a>
             </nav>
         </header>
